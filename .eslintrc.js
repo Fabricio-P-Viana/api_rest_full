2660de497387b0cd3d452e36355d6abdea6d55ad
Fabricio Pereira Viana <125505755+Fabricio-P-Viana@users.noreply.github.com>
2024-01-27 10:09:33 -0300

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'class-methods-use-this': 'off',
+    'no-console': 'off',
+    'import/first': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+  },
+};
